5619680af150efbc3883a90d29523c2a7a552ab1
admin <davidtcc@gmail.com>
2018-08-27 23:59:54 +0200

    added more commands ans some info

--- CheetSheet.py
+++ CheetSheet.py
@@ -1,5 +1,7 @@
 from pymxs import runtime as rt
 
+import MaxPlus
+
 #clear listener
 
 rt.clearlistener()
@@ -26,6 +28,10 @@ rt.sphere(radius=50, position=rt.point3(25, 25, 25), segments=8)
 
 rt.select(rt.objects)
 
+# with MaxPlus
+
+MaxPlus.Core.GetRootNode().Children
+
 #clear selection
 
 rt.clearselection()
@@ -38,5 +44,66 @@ print rt.firstItem.name
 
 rt.selection[0].name = 'PorTuCulpa'
 
+#layer.nodes &thenodes
+
+rt.layer.nodes(pymxs.mxsreference(theNodes))
+
+#autosave preferences
+
+autosave = rt.autosave
+print "Autosave enabled? {}".format(autosave.enable)
+print "Autosave filename: {}".format(autosave.filename)
+
+#coordsys setting
+
+rt.toolMode.coordsys(pymxs.runtime.Name("world"))
+
+#get coordsys value
+
+rt.getRefCoordSys()
 
 
+#execute maxscript code
+
+MaxPlus.Core.EvalMAXScript("coordsys parent $'%s'.pos = Point3 0 0 0" % node_name)
+
+
+#nesting sublayer
+
+parent = rt.LayerManager.NewLayerFromName("C")
+child = rt.LayerManager.NewLayerFromName("D")
+child.setParent(parent)
+
+
+#get key info
+
+
+bone = MaxPlus.INode.GetINodeByName("Bone001")
+ctrl = bone.GetTMController().GetPositionController()
+print ctrl.GetNumKeys()
+
+
+#set attributtes in nested attr
+
+teapot = rt.teapot()
+object = teapot
+attribute = "controller.position.x" #it works with 'controller.pos.x' as well
+attribute_parts = attribute.split(".")
+#~ for a in attribute_parts[:-1]:
+	#~ object = getattr(object, a)
+	
+#~ print "posX: {0}".format(getattr(object, attribute_parts[-1]))
+# prints: posX: 0.0
+setattr(object, '.'.join(attribute_parts[1:]), 20)
+#~ print "posX: {0}".format(getattr(object, attribute_parts[-1]))
+# prints: posX: 20.0
+
+
+# add a shortcut to python script
+"""
+See the MaxPlus.ActionFactory method. The file demoActionFactory.py shows how to create ActionItems 
+(same as what macroscripts do) in python, it creates a 'Python demo' entry in 'Do something' 
+category which you can then bind a shortcut to in the customize UI dialog. There's also demoMenu.py 
+that shows how to add the ActionItem to a menu.
+
+"""
