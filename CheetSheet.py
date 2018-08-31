499872690918df8b119a983d6d22a935096cf796
admin <davidtcc@gmail.com>
2018-08-31 22:47:30 +0200

    Commands to save custom node tabs

--- CheetSheet.py
+++ CheetSheet.py
@@ -137,3 +137,20 @@ for each in selected_nodes:
 
 	mod = MaxPlus.Factory.CreateObjectModifier(MaxPlus.Class_ID(0x9c92c88, 0x13d466dc))
 	MaxPlus.ModifierPanel.AddToSelection(mod)
+
+#How to pass a custom selection to max
+
+selected_nodes = MaxPlus.SelectionManager.GetNodes()
+
+n = len(selected_nodes)
+
+#create tab for selection and then append chossen reference nodes to it
+newSelection = MaxPlus.INodeTab()
+
+for each in range(0, n):
+	print selected_nodes[each].GetName()
+	if each == 1:
+		newSelection.Append(selected_nodes[each])
+
+MaxPlus.SelectionManager.ClearNodeSelection()
+MaxPlus.SelectionManager.SelectNodes(newSelection)
--- test.py
+++ test.py
@@ -2,15 +2,27 @@ import MaxPlus
 
 #Get Selected nodes
 
-selected_nodes = MaxPlus.SelectionManager.Nodes
+selected_nodes = MaxPlus.SelectionManager.GetNodes()
+
+n = len(selected_nodes)
+
+newSelection = MaxPlus.INodeTab()
+
+for each in range(0, n):
+	print selected_nodes[each].GetName()
+	if each == 1:
+		newSelection.Append(selected_nodes[each])
+
+MaxPlus.SelectionManager.ClearNodeSelection()
+MaxPlus.SelectionManager.SelectNodes(newSelection)
 
 #move nodes
 
 
 #add modifier
 
-for each in selected_nodes:
+# for each in selected_nodes:
 
 
-	mod = MaxPlus.Factory.CreateObjectModifier(MaxPlus.Class_ID(0x9c92c88, 0x13d466dc))
-	MaxPlus.ModifierPanel.AddToSelection(mod)
+# 	mod = MaxPlus.Factory.CreateObjectModifier(MaxPlus.Class_ID(0x9c92c88, 0x13d466dc))
+# 	MaxPlus.ModifierPanel.AddToSelection(mod)
