24aa65e2a9840a3241c478e05817b3fd5506228a
1fTh3n3ls3 <davidtcc@gmail.com>
2018-09-12 19:00:59 +0200

    compensations trys

---
+++ mathOp.py
@@ -0,0 +1,18 @@
+import math
+import MaxPlus
+
+# def module(vector):
+# 	return math.sqrt(vector[0] ** 2 + vector[1] ** 2 + vector[2] ** 2)
+
+# print module([1, 1, 1])	
+
+# x = 1.0
+# y = 1.0
+# z = 1.0
+
+
+# c = MaxPlus.Point3()
+# c.Set(x, y, z)
+# print c
+
+test = MaxPlus.Core.EvalMAXScript("2+2")
--- moreTestInPython.py
+++ moreTestInPython.py
@@ -1,5 +1,6 @@
 import MaxPlus
 import copy # module to make copys and deep copies
+import math
 
 # triObjectID = MaxPlus.Class_ID(0x0009, 0)
 
@@ -132,46 +133,59 @@ def AnimateTransform(nodes, tm_list, offset = 0):
 	# Turn off the AutoKey button
 	anim.SetAnimateButtonState(False)
 
-def AnimateTransformCompensation(nodes, tm_list, offset = 0):
-	'''Moves the node around to demonstrate animation'''
-	# select the node so we will see the keyframes in the timeslider
+def AnimateTransformCompensation(nodes, tm_list, offset=0, stride=1.0): # la tm_list ahora mismo se compone de una lista que representa cada frame, que a su vez contiene la informacion de todos los nodos
+	'''Moves the node around to demonstrate animation
 
+	'''
 	ticks_per_frame = 160
 	anim = MaxPlus.Animation
+	initial_tm = tm_list[0][0] #posicion inicial para el padre de toda la jerarquia del biped
+	invert_initial_tm = inverseMatrix(initial_tm)
+	initial_pos = tm_list[0][0].GetTranslation()
+
+	original_pos = nodes[0].GetWorldPosition(offset * ticks_per_frame ) # toma la posicion original en el primer frame 
+
+
 	# Turn on the AutoKey button
 	anim.SetAnimateButtonState(True)
 	for frame in xrange(offset, len(tm_list) + offset):
-		# print frame
-		# anim.SetTime((frame * ticks_per_frame) + offset, False) 
-		t = ((frame + offset)* ticks_per_frame) 
+		t = ((frame)* ticks_per_frame) 
  		for i in xrange(0, len(nodes)):
  			if i > 0:
  				name = (nodes[i].GetName())
-
- 				# if name.find("Hand") == -1:
 				p = nodes[i].GetWorldPosition(t)
-
 				tm_from_file = tm_list[frame - offset][i]
-
 				new_tm = PreservePosition(p, tm_from_file)
+				nodes[i].SetWorldTM(new_tm, t)
+				
+			else:
+				# new_tm = applyOffset(-original_pos, applyOffset(initial_pos, tm_list[frame - offset][i]))
+				new_tm = tm_list[frame - offset][i]
+
+
+				# extraigo la translacion para compensarla por las dimensiones del personaje y volver a reintroducirla
+				p = new_tm.GetTranslation() 
+				# p.X = p.X * stride
+				# p.Y = p.Y * stride
+				# p.Z = p.Z * stride
+
+				new_tm.SetTranslation(p)
 
 				nodes[i].SetWorldTM(new_tm, t)
-
-			else:
-				nodes[i].SetWorldTM(tm_list[frame - offset][i], int(t))
 
 	# Turn off the AutoKey button
 	anim.SetAnimateButtonState(False)	
 
 
-def GetTransformAnim(nodes):
+def GetTransformAnim(nodes, start=None, end=None):
 	'''Moves the node around to demonstrate animation'''
 	# select the node so we will see the keyframes in the timeslider
 	ticks_per_frame = 160
 	# node.Select()
 	anim = MaxPlus.Animation
 
-	start, end = GetAnimationRanges()
+	if start == None or end == None:
+		start, end = GetAnimationRanges()
 
 	range_list = []
 
@@ -218,6 +232,12 @@ def PreservePosition(pos, matrix):
 	tm.SetTranslation(pos)
 	return tm  
 
+def applyOffset(new_pos, matrix):
+	tm = MaxPlus.Matrix3(*matrix)
+	pos = tm.GetTranslation()
+	tm.SetTranslation(pos-new_pos)
+	return tm 	
+
 def getHierarchy(node):
 	hierarchy = []
 	hierarchy.append(node)
@@ -229,6 +249,45 @@ def getHierarchy(node):
 			childs.remove(each)
 			childs.extend(each.Children)
 	return hierarchy
+
+
+def module(vector):
+	return math.sqrt(vector.GetX() ** 2 + vector.GetY() ** 2 + vector.GetZ() ** 2)
+
+def GetDistance(point_a, point_b):	
+	x = point_b.GetX() - point_a.GetX()
+	y = point_b.GetY() - point_a.GetY()
+	z = point_b.GetZ() - point_a.GetZ()
+
+	c = MaxPlus.Point3()
+	c.Set(x, y, z)
+
+	return module(c)
+
+
+def GetLegDistance(hierarchy):
+	foot_node = None
+	calf_node = None
+	thigh_node = None
+	for each in hierarchy:
+		name = each.GetName()
+		if name.find("L Foot") != -1:
+			foot_node = each
+			print "llego1"
+		if name.find("L Calf") != -1:
+			calf_node = each
+			print "llego2"
+		if name.find("L Thigh") != -1:
+			thigh_node = each
+			print "llego3"
+
+	leg_distance = GetDistance(thigh_node.GetWorldPosition(), calf_node.GetWorldPosition()) + GetDistance(foot_node.GetWorldPosition(), calf_node.GetWorldPosition())
+
+	return leg_distance
+
+def StrideProportion(long_leg2, long_leg1):
+	proportion = long_leg2 / long_leg1
+	return proportion	
 
 MaxPlus.Core.EvalMAXScript("clearListener()")
 
@@ -243,6 +302,9 @@ MaxPlus.Core.EvalMAXScript("clearListener()")
 # AnimateTransform(the_node, tm_list)
 
 # TODO: get hierarchy of  biped without nubs
+# TODO: keep initial pose of target biped
+# TODO: work idea about different namespace for mapping
+# TODO: biped class
 
 
 # local_transform = getTransformLocal(the_node)
@@ -250,54 +312,14 @@ MaxPlus.Core.EvalMAXScript("clearListener()")
 # the_node.SetWorldTM(local_transform)
 
 
+
+
+
 ##################
 # get hierarchy
 ##################
 
-# hierarchy_nodes = getHierarchy(*selected_nodes)
-# print len(hierarchy_nodes)
-
-# for each in hierarchy_nodes:
-# 	print each.GetName()
-
-
-
-# tm = the_node.GetWorldTM()
-# inverse_tm = MaxPlus.Matrix3(*tm)
-
-# print tm
-# print inverse_tm
-# inverse_tm.Invert()
-# print inverse_tm
-
-# the_node2 = MaxPlus.INode.GetINodeByName("Bip001")
-# world_tm = the_node2.GetWorldTM()
-# local_tm = getTransformLocal(the_node2)
-
-# world_translation = world_tm.GetTranslation()
-# world_rotation = world_tm.GetRotation()
-
-# translation = local_tm.GetTranslation()
-# quat = local_tm.GetRotation()
-
-# print local_tm
-# print quat
-
-# the_node3 = MaxPlus.INode.GetINodeByName("Bip002 R Forearm")
-
-
-# the_node3.SetLocalRotation(quat)
-# the_node3.SetLocalPosition(translation)
-# the_node3.SetLocalTM(local_tm)
-
-# the_node3.SetWorldTM(world_tm)
-
-
 # TODO check undos 
-
-# MaxPlus.Core.EvalMAXScript("biped.getTransform $'%s' #pos" % the_node3.GetName())
-# MaxPlus.Core.EvalMAXScript("biped.setTransform $'%s' #pos " % the_node3.GetName())
-
 
 ###################################
 # test copy animation with offset
@@ -344,12 +366,23 @@ hierarchy_bip1 = getHierarchy(bip001)
 bip002 = MaxPlus.INode.GetINodeByName("Bip002")
 hierarchy_bip2 = getHierarchy(bip002)
 
+# the_tm = GetTransformAnim(hierarchy_bip1, start=1137, end=1497)
 the_tm = GetTransformAnim(hierarchy_bip1)
 
 
-newFrames = 125 * 160
-newRange = MaxPlus.Interval(0, newFrames)
-MaxPlus.Animation.SetRange(newRange)
+
+ld1 = GetLegDistance(hierarchy_bip1)
+ld2 = GetLegDistance(hierarchy_bip2)
+
+sp = StrideProportion(ld2, ld1)
+
+print sp
+
+
+
+# newFrames = 125 * 160
+# newRange = MaxPlus.Interval(0, newFrames)
+# MaxPlus.Animation.SetRange(newRange)
 
 bip1_tm = bip001.GetWorldTM()
 
@@ -359,8 +392,10 @@ bip2_tm = bip002.GetWorldTM()
 
 bip2_tm.Invert()
 
-AnimateTransformCompensation(hierarchy_bip2, the_tm, offset=10)
-# AnimateTransform(hierarchy_bip2, the_tm, offset=10)
+AnimateTransformCompensation(hierarchy_bip2, the_tm, offset=0, stride=sp)
+# AnimateTransformCompensation(hierarchy_bip2, the_tm, offset=0)
+
+# AnimateTransform(hierarchy_bip2, the_tm, offset=0)
 
 
 # MaxPlus.Core.EvalMAXScript("timeSlider.setVisible true")
@@ -368,8 +403,9 @@ AnimateTransformCompensation(hierarchy_bip2, the_tm, offset=10)
 # MaxPlus.Core.EvalMAXScript("enableSceneRedraw()")
 
 # TODO: ver si se puede ser consistente descomponiendo el pegado en posicion y rotacion
-
-
-
+# FIX: que pasa con los dedos
+# TODO: pillar valor del maxscript ejecutado
+# TODO: contemplar la posibilidad de que el bip este linkado a un master
+# TODO: mirror
 
 
