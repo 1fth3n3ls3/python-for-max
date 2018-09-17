46c6d4a8d4586035777636e42e9e7fc1508326a0
1fTh3n3ls3 <davidtcc@gmail.com>
2018-09-17 10:24:03 +0200

    add table

--- tes3.py
+++ tes3.py
@@ -40,61 +40,85 @@ for each in x:
 
 # print "test{}".format("ddd")
 
-def GetHierarchyTrees(nodes):
+def CreateIdList(nodes):
 	list_nodes = list(nodes)
-	print list_nodes
+	# print list_nodes
 	list_names = list(each.GetName() for each in list_nodes)
-	print list_names
-
+	# print list_names
+	tuple_list = []
 	i = 0
-  	for each in  list_nodes:
-  		parent = each.GetParent()
-  		parent_name = parent.GetName()
-  		if  parent_name in list_names:
-  			print parent_name  
-  		
+	for each in  list_nodes:
+		parent = each.GetParent()
+		parent_name = parent.GetName()
 
-		# parent = each.GetParent()
-		# parent_name = parent.GetName()
-		# print each
-		# print parent
-		# if parent in list(nodes):
-		# 	print parent
+		if  parent_name in list_names:
+			tuple_list.append((i, list_names.index(parent_name)))
+
+		else :
+			tuple_list.append((i, i))
+		
+		i += 1
+
+
+	return tuple_list
+
+
+def GetHierarchyTree(ids_list, node_list):
+	# pass 1: create nodes dictionary
+	nodes = {}
+	for i in ids_list:
+		id, parent_id = i
+		nodes[id] = { 'id': id }
+
+	# pass 2: create trees and parent-child relations
+	forest = []
+	for i in ids_list:
+		id, parent_id = i
+		node = nodes[id]
+
+		# either make the node a new tree or link it to its parent
+		if id == parent_id:
+			# start a new tree in the forest
+			forest.append(node)
+		else:
+			# add new_node as child to parent
+			parent = nodes[parent_id]
+			if not 'children' in parent:
+				# ensure parent has a 'children' field
+				parent['children'] = []
+			children = parent['children']
+			children.append(node)
+
+	return forest
+
+
+
 
 MaxPlus.Core.EvalMAXScript("clearlistener()")
 
 selected_nodes = MaxPlus.SelectionManager.Nodes
 
-GetHierarchyTrees(selected_nodes)
+ids_list =  CreateIdList(selected_nodes)
+		
+
+trees =  GetHierarchyTree(ids_list, selected_nodes)
 
 
-a = [(1, 1), (2, 1), (3, 1), (4, 3), (5, 3), (6, 3), (7, 7), (8, 7), (9, 7)] # first number is id, second parent id, if id is eaqual to idparent the object is root.
+for tree in trees:
+	children = False
+	# print each['id']
+	if 'children' in  tree.keys():
+		children = True
+		# while children:
+		for branch in tree['children']:
+			print branch
+			if 'children' in  branch.keys():
+				branch = branch['children']
 
-# pass 1: create nodes dictionary
-nodes = {}
-for i in a:
-    id, parent_id = i
-    nodes[id] = { 'id': id }
+				print 'llego ' + str(branch)
+			else:
+				children = False
+				print 'llego1'
 
-# pass 2: create trees and parent-child relations
-forest = []
-for i in a:
-    id, parent_id = i
-    node = nodes[id]
 
-    # either make the node a new tree or link it to its parent
-    if id == parent_id:
-        # start a new tree in the forest
-        forest.append(node)
-    else:
-        # add new_node as child to parent
-        parent = nodes[parent_id]
-        if not 'children' in parent:
-            # ensure parent has a 'children' field
-            parent['children'] = []
-        children = parent['children']
-        children.append(node)
-
-print forest
-			
 
